36d9feadfa1d6ad0ce95b3b29fa788970326b9f8
Truong Hoang Minh <hoang-minh.truong@ekino.com>
2024-11-08 15:39:25 +0700

    add SSG

--- apps/badminton-store/next.config.ts
+++ apps/badminton-store/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // allow image host from external
+  images: {
+    domains: ["fakestoreapi.com"],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- apps/phone-store/src/app/page.tsx
+++ apps/phone-store/src/app/page.tsx
@@ -1,9 +1,15 @@
-import { uppercase } from "utils";
+import { uppercase } from "@packages/utils";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Phone store {uppercase("hello")}
+      {products.map((product) => {
+        return <h3 key={product.title}>{product.title}</h3>;
+      })}
     </div>
   );
 }
